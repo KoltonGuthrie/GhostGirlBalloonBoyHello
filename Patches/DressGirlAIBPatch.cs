3e2b4f31ad8f5f4530e3c24eceab5d25f7442663
Kolton Guthrie <69282264+KoltonGuthrie@users.noreply.github.com>
2024-01-27 04:18:59 -0600

    Initial Commit

---
+++ Patches/DressGirlAIBPatch.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace GhostGirlBalloonBoyHello.Patches
+{
+    [HarmonyPatch(typeof(DressGirlAI))]
+    internal class DressGirlAIPatch
+    {
+        private static int lastSeenAmount = 0;
+
+        [HarmonyPatch(typeof(DressGirlAI), "Update")]
+        [HarmonyPostfix]
+        [HarmonyWrapSafe]
+        static void patchUpdate(DressGirlAI __instance)
+        {
+            int __timesSeenByPlayer = Traverse.Create(__instance).Field("timesSeenByPlayer").GetValue<int>();
+            if (__timesSeenByPlayer == 0)
+            {
+                lastSeenAmount = 0;
+                return;
+            }
+            if(__timesSeenByPlayer > lastSeenAmount)
+            {
+                lastSeenAmount = __timesSeenByPlayer;
+
+                AudioSource audioSource = __instance.GetComponent<AudioSource>();
+                audioSource.maxDistance = 50f;
+                audioSource.Stop();
+                audioSource.PlayOneShot(GhostGirlBalloonBoyHelloBase.SoundFX[0]);
+
+            }
+            
+        }
+
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,56 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using GhostGirlBalloonBoyHello.Patches;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace GhostGirlBalloonBoyHello
+{
+
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class GhostGirlBalloonBoyHelloBase : BaseUnityPlugin
+    {
+        private const string modGUID = "Kolton12O.GhostGirlBalloonBoyHello";
+        private const string modName = "GhostGirlBalloonBoyHello";
+        private const string modVersion = "1.0.2";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        public static GhostGirlBalloonBoyHelloBase Instance;
+
+        internal ManualLogSource mls;
+
+        internal static List<AudioClip> SoundFX;
+        internal static AssetBundle Bundle;
+
+        void Awake()
+        {
+            if(Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("The Ghost Girl Balloon Boy Hello has awaken!");
+
+            harmony.PatchAll(typeof(DressGirlAIPatch));
+
+            SoundFX = new List<AudioClip>();
+            string FolderLocation = Instance.Info.Location;
+            FolderLocation = FolderLocation.TrimEnd((modName + ".dll").ToCharArray());
+            Bundle = AssetBundle.LoadFromFile(FolderLocation + "balloonboyhello");
+
+            if (Bundle != null)
+            {
+                mls.LogInfo("Successfully loaded asset bundle! :)");
+                SoundFX = Bundle.LoadAllAssets<AudioClip>().ToList();
+            } else
+            {
+                mls.LogError("Failed to load asset bundle! :(");
+            }
+        }
+    }
+}
